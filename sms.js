0d91cfb5dc06d2f81c3f5d2a5406c0db459d38e7
mycatatan <camarugulam@gmail.com>
2026-07-05 01:54:40 +0700

    Update sms.js

--- sms.js
+++ sms.js
@@ -5,12 +5,13 @@ import { db } from './firebase.js';
 // 1. KONFIGURASI PROVIDER & STATE
 // ==========================================
 const PROVIDERS = {
+    "smscode": { name: "COD", url: "https://sms.aam-zip.workers.dev" },
     "herosms": { name: "HER", url: "https://hero.hajirnasrullah6.workers.dev" },
     "svco": { name: "SVC", url: "https://svco.hajirnasrullah6.workers.dev" }
  // Ganti dengan URL Worker Nixpoin Anda
 };
 
-let activeProviderKey = localStorage.getItem('xurel_provider') || "herosms";
+let activeProviderKey = localStorage.getItem('xurel_provider') || "smscode";
 let BASE_URL = PROVIDERS[activeProviderKey].url;
 
 let currentServerName = ""; 
